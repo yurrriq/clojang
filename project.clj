5ed22efe01e6a5658b246a6b571f538dfc83725a
Duncan McGreggor <oubiwann@gmail.com>
2015-09-13 23:16:09 -0500

    Renamed to clojang and started with new version number.

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
-(defproject clojure-erlastic "0.2.3"
-  :description "Micro lib making use of erlang JInterface lib to decode and
-                encode Binary Erlang Term and simple erlang port interface with
-                core.async channel. So you can communicate with erlang coroutine
-                with clojure abstraction"
-  :url "https://github.com/awetzel/clojure-erlastic"
-  :scm {:name "git" :url "https://github.com/awetzel/clojure-erlastic"}
-  :license {:name "The MIT License"
-            :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+(defproject clojang "0.3.0"
+  :description "A Clojure wrapper forErlang's JInterface"
+  :url "https://github.com/oubiwann/clojang"
+  :scm {:name "git" :url "https://github.com/oubiwann/clojang"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0"]
                  [org.erlang.otp/jinterface "1.5.9"]]
   :repositories {"scalaris" "https://scalaris.googlecode.com/svn/maven/"})
