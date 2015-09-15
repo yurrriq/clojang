6b8bec2c2557b469f13f8a1690a57357d8710006
Duncan McGreggor <oubiwann@gmail.com>
2015-09-14 23:37:40 -0500

    Added new types module.

--- src/clojang/core.clj
+++ src/clojang/core.clj
@@ -1,39 +1,16 @@
-(ns clojure-erlastic.core)
-(import '(com.ericsson.otp.erlang OtpErlangAtom OtpErlangObject OtpErlangTuple
-                                  OtpErlangString OtpErlangList OtpErlangLong
-                                  OtpErlangBinary OtpErlangDouble OtpErlangMap
-                                  OtpErlangDecodeException OtpInputStream OtpOutputStream OtpException))
-(require '[clojure.core.async :as async :refer [<! >! <!! chan go close!]])
+(ns clojang.core
+  (:require [clojure.core.async :as async :refer [<! >! <!! chan go close!]]
+            [clojang.types :as types]
+            [trptcolin.versioneer.core :as version])
+  (:import [com.ericsson.otp.erlang
+            OtpErlangDecodeException
+            OtpInputStream
+            OtpOutputStream
+            OtpException])
+  (:gen-class))
 
-(defn decode [obj]
-  (let [t (type obj)]
-    (cond 
-      (= t OtpErlangAtom) (let [k (keyword (.atomValue obj))]
-                              (if (#{:true :false} k) (= k :true) k))
-      (= t OtpErlangList) (seq (map decode (.elements obj)))
-      (= t OtpErlangTuple) (vec (map decode (.elements obj)))
-      (= t OtpErlangString) (.stringValue obj)
-      (= t OtpErlangBinary) (.binaryValue obj)
-      (= t OtpErlangMap) (zipmap (map decode (.keys obj)) (map decode (.values obj)))
-      (= t OtpErlangLong) (.longValue obj)
-      (= t OtpErlangDouble) (.doubleValue obj)
-      :else   :decoding_error)))
-
-(defn encode [obj]
-  (cond
-    (nil? obj) (OtpErlangAtom. "nil")
-    (seq? obj) (OtpErlangList. (into-array OtpErlangObject (map encode obj)))
-    (set? obj) (OtpErlangList. (into-array OtpErlangObject (map encode obj)))
-    (vector? obj) (OtpErlangTuple. (into-array OtpErlangObject (map encode obj)))
-    (string? obj) (OtpErlangBinary. (bytes (.getBytes obj "UTF-8")))
-    (keyword? obj) (OtpErlangAtom. (name obj))
-    (integer? obj) (OtpErlangLong. (long obj))
-    (float? obj) (OtpErlangDouble. (double obj))
-    (map? obj) (OtpErlangMap. (into-array OtpErlangObject (map encode (keys obj))) 
-                              (into-array OtpErlangObject (map encode (vals obj))))
-    (= (Class/forName "[B") (type obj)) (OtpErlangBinary. (bytes obj))
-    (= java.lang.Boolean (type obj)) (encode (keyword (str obj)))
-    :else   (encode (str obj))))
+(defn get-version []
+  (version/get-version "" "clojang"))
 
 (defn log [& params]
   (.println System/err (apply str params)))
@@ -48,7 +25,7 @@
             (let [term-len (.read4BE (new OtpInputStream len-buf))
                   term-buf (byte-array term-len)]
               (.read System/in term-buf)
-              (let [b (decode (.read_any (new OtpInputStream term-buf))) ]
+              (let [b (types/decode (.read_any (new OtpInputStream term-buf))) ]
                 (>! in b)))))
         (catch Exception e (do 
           (log "receive error : " (type e) " " (.getMessage e)) 
@@ -57,7 +34,7 @@
       (loop []
         (when-let [term (<! out)]
           (try
-            (let [out-term (new OtpOutputStream (encode term))]
+            (let [out-term (new OtpOutputStream (types/encode term))]
               (doto (new OtpOutputStream) (.write4BE (+ 1 (.size out-term))) (.write 131) (.writeTo System/out))
               (.writeTo out-term System/out)
               (.flush System/out))
---
+++ src/clojang/types.clj
@@ -0,0 +1,176 @@
+(ns clojang.types
+  (:import [com.ericsson.otp.erlang
+            OtpErlangAtom
+            OtpErlangBinary
+            OtpErlangBitstr
+            OtpErlangDouble
+            OtpErlangFloat
+            OtpErlangByte
+            OtpErlangChar
+            OtpErlangShort
+            OtpErlangUShort
+            OtpErlangLong
+            OtpErlangInt
+            OtpErlangUInt
+            OtpErlangList
+            OtpErlangPid
+            OtpErlangPort
+            OtpErlangRef
+            OtpErlangFun
+            OtpErlangExternalFun
+            OtpErlangTuple
+            ;;OtpErlangMap
+            OtpErlangObject
+            OtpErlangString
+            OtpErlangBoolean])
+  (:gen-class))
+
+(declare encode decode)
+
+;;; atoms
+
+(defmulti erl-atom class)
+(defmethod erl-atom String [s] :a-string)
+(defmethod erl-atom clojure.lang.Keyword [s] :a-keyword)
+
+(defn erl-atom [str]
+  (OtpErlangAtom. str))
+
+(defn erl-atom [kwd]
+  (OtpErlangAtom. (name kwd)))
+
+(defn erl-atom? [x]
+  (= (type x) OtpErlangAtom))
+
+(defn ->atom [obj]
+  (let [k (keyword (.atomValue obj))]
+    (cond
+      (#{:true :false} k) (= k :true)
+      (#{:nil :null :none :undefined} k) nil
+      :else k)))
+
+;;; tuples
+
+(defn erl-tuple [vec]
+  (OtpErlangTuple. (into-array OtpErlangObject (map encode vec))))
+
+(defn erl-tuple? [x]
+  (= (type x) OtpErlangTuple))
+
+(defn ->tuple [obj]
+  (vec (map decode (.elements obj))))
+
+;;; strings
+
+(defn erl-str [str]
+  (OtpErlangString. str))
+
+(defn erl-str? [x]
+  (= (type x) OtpErlangString))
+
+(defn ->str [erl]
+  (.stringValue erl))
+
+;;; lists
+
+(defn erl-list [lst]
+  (OtpErlangList. (into-array OtpErlangObject (map encode lst))))
+
+(defn erl-list? [x]
+  (= (type x) OtpErlangList))
+
+(defn ->list [obj]
+  (seq (map decode (.elements obj))))
+
+;;; maps
+
+;; (defn erl-map [a-map]
+;;   (OtpErlangMap. (into-array OtpErlangObject (map encode (keys a-map))) 
+;;                  (into-array OtpErlangObject (map encode (vals a-map)))))
+
+;; (defn erl-map? [x]
+;;   (= (type x) OtpErlangMap))
+
+;; (defn ->map [obj]
+;;   (zipmap (map decode (.keys obj))
+;;           (map decode (.values obj))))
+
+;;; longs
+
+(defn erl-long [lng]
+  (OtpErlangLong. (long lng)))
+
+(defn erl-long? [x]
+  (= (type x) OtpErlangLong))
+
+(defn ->long [obj]
+  (.longValue obj))
+
+;;; doubles
+
+(defn erl-double [dbl]
+  (OtpErlangDouble. (double dbl)))
+
+(defn erl-double? [x]
+  (= (type x) OtpErlangDouble))
+
+(defn ->double [obj]
+  (.doubleValue obj))
+
+;;; bytes
+
+(defn erl-bytes [bytes]
+  (OtpErlangBinary. (bytes bytes)))
+
+;;; booleans
+
+(defn erl-bool [bool]
+  (OtpErlangBoolean. bool))
+
+;;; binaries
+
+(defn erl-bin [bin]
+  (OtpErlangBinary. (bytes (.getBytes bin "UTF-8"))))
+
+(defn erl-bin? [x]
+  (= (type x) OtpErlangBinary))
+
+(defn ->bin [obj]
+  (.binaryValue obj))
+
+;;; Convenience functions for Clojure types
+
+(defn bool? [x]
+  (= java.lang.Boolean (type x)))
+
+(defn bytes? [x]
+  (= (Class/forName "[B") (type x)))
+
+;;; Coder functions
+
+(defn decode [obj]
+  (cond 
+    (erl-atom? obj) (->atom obj)
+    (erl-list? obj) (->list obj)
+    (erl-tuple? obj) (->tuple obj)
+    (erl-str? obj) (->str obj)
+    (erl-bin? obj) (->bin obj)
+    ;;(erl-map? obj) (->map obj)
+    (erl-long? obj) (->long obj)
+    (erl-double? obj) (->double obj)
+    :else   :decoding_error))
+
+(defn encode [obj]
+  (cond
+    (nil? obj) (erl-atom :nil)
+    (seq? obj) (erl-list obj)
+    (set? obj) (erl-list obj)
+    (vector? obj) (erl-tuple obj)
+    (string? obj) (erl-bytes obj)
+    (keyword? obj) (erl-atom obj)
+    (integer? obj) (erl-long obj)
+    (float? obj) (erl-double obj)
+    ;; (map? obj) (erl-map obj)
+    (bytes? obj) (erl-bytes obj)
+    (bool? obj) (erl-bool obj)
+    :else   (encode (str obj))))
